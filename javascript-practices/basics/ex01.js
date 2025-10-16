e23790a0913b1bc3ad1358327db476ef3854acac
kimjuhan-95 <jhkim9596@gmail.com>
2025-10-16 09:48:32 +0900

    asd

---
+++ javascript-practices/basics/ex01.js
@@ -0,0 +1,44 @@
+console.log("====== [01] 기본 타입, primitive type");
+var u;
+var i = 10;
+var s = "hello world";
+var b = true;
+
+
+console.log("u:" + typeof(u) + ":"  + u);
+console.log("i:" + typeof(i) + ":"  + i);
+console.log("s:" + typeof(s) + ":"  + s);
+console.log("b:" + typeof(b) + ":"  + b);
+
+console.log("====== [02.01] 객체 타입, object");
+var i = new Number(10);
+var s = new String("hello world");
+var b = new Boolean(false);
+var o = {};
+var a = [];
+var n = null;
+
+console.log("i:" + typeof(i) + ":" + (i instanceof(Number)));
+console.log("s:" + typeof(s) + ":" + (s instanceof(String)));
+console.log("b:" + typeof(b) + ":" + (b instanceof(Boolean)));
+console.log("o:" + typeof(o) + ":" + (o instanceof(Object)));
+console.log("a:" + typeof(a) + ":" + (a instanceof(Array)));
+console.log("n:" + typeof(n));
+
+console.log("====== [02.02] 객체 타입, function");
+var f1 = function() {
+}
+function f2() {
+}
+var f3 = new Function();
+
+console.log("f1:" + typeof(f1) + ":" + (f1 instanceof(Function)))
+console.log("f2:" + typeof(f2) + ":" + (f2 instanceof(Function)));
+console.log("f3:" + typeof(f3) + ":" + (f3 instanceof(Function)));
+
+console.log("====== [03] 기본 타입과 유사객체");
+var b1 = true;                 // new Boolean(true)  x
+var b2 = new Boolean(true);
+
+console.log(b1.valueOf());     // new Boolean(b1).valueOf() o
+console.log(b2.valueOf());
