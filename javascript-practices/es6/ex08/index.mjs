4f1add010f2ed081b3488d352ae95e63bf320d09
kimjuhan-95 <jhkim9596@gmail.com>
2025-10-22 12:32:49 +0900

    asd

---
+++ javascript-practices/es6/ex08/index.mjs
@@ -0,0 +1,19 @@
+console.log("=========[01. Unnamed Export Module]");
+
+import myFunction from "./mods/ex01.mjs";
+console.log(myFunction(10, 20));
+
+import myObject from "./mods/ex02.mjs";
+console.log(myObject.add(10, 20), myObject.subtract(100, 200));
+
+console.log("=========[02. Named Export ModuleI]");
+import {subtract} from "./mods/ex03.mjs";
+console.log(subtract(100, 200));
+
+console.log("=========[03. Named Export ModuleII]");
+import * as m from "./mods/ex03.mjs";
+console.log(m.add(10, 20), m.subtract(100, 200));
+
+console.log("=========[04. Named + Unnamed Export ModuleII]");
+import math, {add} from './mods/ex04.mjs';
+console.log(math.subtract(100, 200), add(10, 20));
